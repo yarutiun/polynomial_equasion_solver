a3eaa51fc0f84ec4a22a6b8adfadc935eff17f7c
yarutiun <y.arutiunofff@gmail.com>
2024-09-26 20:46:48 +0200

    some minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 double findDescriminant(int a, int b, int c) {
     double res = pow(b, 2) - 4 * a * c;
+    cout << "The Descriminant is: " <<  res << endl;
     if(res == 0) {
         cout << "The equation has one real root" << endl;
         return 0;
@@ -12,6 +13,7 @@ double findDescriminant(int a, int b, int c) {
     } else {
         cout << "The equation has no real roots" << endl;
     }
+    return res;
 }
 
 vector<double> getCoefficients(string equation) {
@@ -44,11 +46,15 @@ void solveQuadratic(int a, int b, int c) {
 }
 
 int main(int argc, char **argv) {
-    //2x^2 + 3x + 1 = 0
     if(argc == 2) {
         string arg = argv[1];
-        cout << findDegree(getCoefficients(arg)) << endl;
-        solveQuadratic(2, 3, 1);
+        if(findDegree(getCoefficients(arg)) > 2) {
+            cout << "The polynomial degree is stricly greater than 2, I can't solve." << endl;
+        }
+        if(findDegree(getCoefficients(arg)) == 2) {
+            cout << "Reduced form: " << arg << endl;
+            solveQuadratic(1, 2, 1);    
+        }
         return 0;
     }
     cout << "Wrong number of arguments" << endl;
